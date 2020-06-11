9c3f6ea00fd20bd9a9bee23f20733d0466a0d2ed
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2020-06-11 03:09:34 -0400

    avanzando cards

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackplugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.html'),
+  output: {
+    filename: 'bundle.html'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+        
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        loader: 'file-loader',
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackplugin({
+      template: path.resolve(__dirname, './src/index.html')
+    })
+  ]
+}
