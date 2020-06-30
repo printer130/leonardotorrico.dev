5e48ac445377f03a2e83e17b2467d40b88d30d27
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2020-06-30 01:56:08 -0400

    env

--- api/config/index.js
+++ api/config/index.js
@@ -2,11 +2,15 @@
 // import dotenv from 'dotenv'
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-
+// export const configg = {
+//   dbName: Deno.env.get('DB_NAME'),
+//   dbPassword: Deno.env.get('DB_PASSWORD'),
+//   dbUsername: Deno.env.get('DB_USERNAME'),
+//   port: Deno.env.get('PORT'),
+// }
 export const configg = {
-  dbName: Deno.env.DB_NAME,
-  dbPassword: Deno.env.DB_PASSWORD,
-  dbUsername: Deno.env.DB_USERNAME,
-  port: Deno.env.PORT,
+  dbName: config().DB_NAME,
+  dbPassword: config().DB_PASSWORD,
+  dbUsername: config().DB_USERNAME,
+  port: config().DB_PORT
 }
-
